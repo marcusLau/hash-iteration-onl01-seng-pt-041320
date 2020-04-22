699eec0a545b299bf2275a1bc158507f9ce97c2d
Marcus Lau <lau.marcus76@gmail.com>
2020-04-22 22:26:51 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -16,7 +16,8 @@ def age_appropriate_birthday(birthday_kids)
     if age < 12
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"  
     end
-  else
-    puts "You are too old for this."
+    else
+      puts "You are too old for this."
+    end
   end
 end
